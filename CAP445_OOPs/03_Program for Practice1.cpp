5522c6f2f8d77e56edf18cb89fe8e6d66113c23f
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-09-14 11:07:42 +0530

    31augclass

---
+++ CAP445_OOPs/03_Program for Practice1.cpp
@@ -0,0 +1,11 @@
+/*
+A doctor is giving advice to patient about how to maintain our body which type of food we should take. For that doctor asks for the number of calories and fat grams in a food. Then it displays the percentage of calories that come from fat. If the calories from fat are less then 30% of the total calories of the food, it should also display a message indicating that the food is low in fat.
+Note:- Note: consider one gram of fat has 9 calories
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
---
+++ CAP445_OOPs/C1Structue_max_min_Array.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+struct MyStructure
+{
+    int num[10], i;
+    int getMax()
+    {
+        cout<<" enter 10 Number"<<endl;
+        for (i = 0; i < 10; i++)
+        {
+            cin>>num[i];   
+        }   
+        cout<<"Array are :"<<endl;
+        for (i = 0; i < 10; i++)
+        {
+            cout<< num[i]<< " ";
+        } 
+            int max=num[0];
+            for ( i = 1; i < 10; i++)
+            {
+            if (max<num[i])
+            {
+                max=num[i];
+            }
+            }
+            int min=num[0];
+            for ( i = 0; i < 10; i++)
+            {
+            if (min>num[i])
+            {
+                min=num[i];
+            }
+            }
+        cout<<"Maximun value :"<<max<<endl;
+        cout<<"Minimun value :"<<min<<endl;
+    } 
+};
+
+int main(){
+    struct MyStructure S1;
+    S1.getMax();
+    return 0;
+}
