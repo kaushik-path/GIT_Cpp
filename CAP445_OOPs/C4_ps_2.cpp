069aefbf392ccd9bd5ef006468dbf99fed588914
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-09-28 11:10:40 +0530

    2D array

---
+++ CAP445_OOPs/C4_ps_2.cpp
@@ -0,0 +1,32 @@
+/* 2D-Array Problem Statement */
+
+#include<iostream>
+using namespace std;
+
+// class 2Darray{
+//     public:
+//     int marks[4][3];
+//     void setter(){
+//         cout<<"Enter reading Values"<<endl;
+//         for (int i = 0; i < 4; i++)
+//         {
+//             cout<<"e"
+//         }
+        
+//     }
+// };
+int main(){
+    
+    int mark[4][3];
+    for (int i = 0; i < 4; i++)
+    {
+        for ( int j = 0; j < 3; i++)
+        {
+            cout<<"enter Marks for the subject"<<j+1<<endl;
+            cin>>mark[i][j];
+        }
+        
+    }
+    
+    return 0;
+}
