9e008090b6d99e9574d0e315a6d3ee48c2508c64
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-10-05 11:42:00 +0530

    Constructor and Destructor

---
+++ CAP445_OOPs/C6_ps_1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class MyClass
+{
+    public:
+    int i,j;
+
+    MyClass(int a, int b){
+        i=a;
+        j=b;
+        cout<<"Constructor Invoked"<<endl;  
+    }
+    void getdata(){
+        cout<<i<<endl;
+        cout<<j<<endl;
+        int sum = i+j;
+        cout<<sum<<endl;
+    }
+    ~MyClass(){
+        cout<<"Destructor Invoked"<<endl;
+    }
+};
+
+
+int main(){
+    
+    int x, y;
+    cin>>x>>y;
+    MyClass m1(x,y);
+    m1.getdata();
+    
+    return 0;
+}
