daa9a185120ac59d09a617b4af37ae84f2da2f38
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-11-11 21:55:55 +0530

    CA question no 2

---
+++ Basic Ques CPP/CA2Ques.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i, pn=0, nn=0;
+//cout<< "Enter the size of array"<<endl;
+//cin>>s;
+int arr[7];
+for(i=0 ; i<7 ; i++)
+{
+    cin>>arr[i];
+   if(arr[i]<0) // 1 2 3 -5 -9 -8 4
+        nn++; // 3
+   else
+        pn++; // 4
+}
+cout<< "Negative numbers = "<<nn<<endl;
+cout<< "Positive numbers = "<<pn;
+return 0;
+}
